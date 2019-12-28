00bf11ec4d12dd3e71d959f3f6de4298db0b86ec
lmm66666 <2586709056@qq.com>
2019-12-28 14:16:56 +0800

    2.0
    。。。

--- pages/DIYPage/DIYPage.js
+++ pages/DIYPage/DIYPage.js
@@ -4,38 +4,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    button1Text: "上传图片",
+    button2Text: "选择瓷砖",
+    picPath: "/images/biscuit.png",
+    width: 85,
+    rotate: [{num: 0,}, {num: 0,}, {num: 0,}, {num: 0,}, {num: 0,}, {num: 0,}, {num: 0,}, {num: 0,}, 
+             {num: 0,}, {num: 0,}, {num: 0,}, {num: 0,}, {num: 0,}, {num: 0,}, {num: 0,}, {num: 0,}]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.initialCanvas()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function (e) {
-    var context = wx.createCanvasContext('firstCanvas')
-
-    context.setStrokeStyle("#00ff00")
-    context.setLineWidth(5)
-    context.rect(0, 0, 200, 200)
-    context.stroke()
-    context.setStrokeStyle("#ff0000")
-    context.setLineWidth(2)
-    context.moveTo(160, 100)
-    context.arc(100, 100, 60, 0, 2 * Math.PI, true)
-    context.moveTo(140, 100)
-    context.arc(100, 100, 40, 0, Math.PI, false)
-    context.moveTo(85, 80)
-    context.arc(80, 80, 5, 0, 2 * Math.PI, true)
-    context.moveTo(125, 80)
-    context.arc(120, 80, 5, 0, 2 * Math.PI, true)
-    context.stroke()
-    context.draw()
+  onReady: function () {
+  
   },
 
   /**
@@ -70,18 +58,128 @@ Page({
     
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+
+  btnClicked: function(options){
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths[0]
+        var picPath = that.data.picPath;
+        that.setData({
+          picPath:tempFilePaths
+        })
+        that.initialCanvas()
+      }
+    })
   },
 
-  picClicked : function(){
-
+  initialCanvas: function(){
+    var ctx = wx.createCanvasContext()
+    var text = "picCanvas"
+    var canvasID = ""
+    for (var i = 1; i < 17; i++) {
+      text = "picCanvas"
+      canvasID = text + String(i)
+      ctx = wx.createCanvasContext(canvasID, this)
+      ctx.drawImage(this.data.picPath, 0, 0, this.data.width, this.data.width)
+      ctx.draw()
+    }
   },
 
-  canvasIdErrorCallback: function (e) {
-    console.error(e.detail.errMsg)
+  picClicked: function(e){
+    var n = Number(e.currentTarget.dataset.name)
+    var canvasID = 'picCanvas' + String(n)
+    var num = this.getNum(n)
+    console.log('图片序号：' + n + ' 点击次数：' + (num+1) + ' 图片路径' + this.data.picPath)
+    var ctx = wx.createCanvasContext(canvasID, this)
+    if (num % 4 == 0){
+      ctx.translate(this.data.width, 0)
+    }
+    else if (num % 4 == 1) {
+      ctx.translate(this.data.width, this.data.width)
+    }
+    else if (num % 4 == 2) {
+      ctx.translate(0, this.data.width)
+    }
+    else if (num % 4 == 3) {
+      ctx.translate(0, 0)
+    }
+    ctx.rotate((num+1) * Math.PI / 2)
+    ctx.drawImage(this.data.picPath, 0, 0, this.data.width, this.data.width)
+    ctx.draw()
   },
+
+  getNum: function(n){
+    let num = 0
+    if (n == 1){
+      num = this.data.rotate[0].num
+      this.setData({ 'rotate[0].num': num + 1 })
+    }
+    else if (n == 2) {
+      num = this.data.rotate[1].num
+      this.setData({ 'rotate[1].num': num + 1 })
+    }
+    else if (n == 3) {
+      num = this.data.rotate[2].num
+      this.setData({ 'rotate[2].num': num + 1 })
+    }
+    else if (n == 4) {
+      num = this.data.rotate[3].num
+      this.setData({ 'rotate[3].num': num + 1 })
+    }
+    else if (n == 5) {
+      num = this.data.rotate[4].num
+      this.setData({ 'rotate[4].num': num + 1 })
+    }
+    else if (n == 6) {
+      num = this.data.rotate[5].num
+      this.setData({ 'rotate[5].num': num + 1 })
+    } 
+    else if (n == 7) {
+      num = this.data.rotate[6].num
+      this.setData({ 'rotate[6].num': num + 1 })
+    }
+    else if (n == 8) {
+      num = this.data.rotate[7].num
+      this.setData({ 'rotate[7].num': num + 1 })
+    }
+    else if (n == 9) {
+      num = this.data.rotate[8].num
+      this.setData({ 'rotate[8].num': num + 1 })
+    }
+    else if (n == 10) {
+      num = this.data.rotate[9].num
+      this.setData({ 'rotate[9].num': num + 1 })
+    }
+    else if (n == 11) {
+      num = this.data.rotate[10].num
+      this.setData({ 'rotate[10].num': num + 1 })
+    }
+    else if (n == 12) {
+      num = this.data.rotate[11].num
+      this.setData({ 'rotate[11].num': num + 1 })
+    }
+    else if (n == 13) {
+      num = this.data.rotate[12].num
+      this.setData({ 'rotate[12].num': num + 1 })
+    }
+    else if (n == 14) {
+      num = this.data.rotate[13].num
+      this.setData({ 'rotate[13].num': num + 1 })
+    }
+    else if (n == 15) {
+      num = this.data.rotate[14].num
+      this.setData({ 'rotate[14].num': num + 1 })
+    }
+    else if (n == 16) {
+      num = this.data.rotate[15].num
+      this.setData({ 'rotate[15].num': num + 1 })
+    }
+    return num
+  }
+
 })
--- pages/explorePage/explorePage.js
+++ pages/explorePage/explorePage.js
@@ -4,7 +4,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    swiperPicUrl:[
+      'http://www.monalisa.com.cn/UploadFile/ads/536bcadb-9f8f-4865-8345-9cc02f3de6f8.jpg', 
+      'http://www.monalisa.com.cn//UploadFile/ads/50eb8e33-feb2-4cf6-b134-52d756a1d101.jpg', 
+      'http://www.monalisa.com.cn/UploadFile/ads/f4b5f3a6-9c66-4c26-8c51-633b3b9e119f.jpg', 
+      'https://www.newzhongyuan.com/data/2017/g/www.gdxzytc.kenfor.com//db_file/201907/13/1563012152811114.jpg',
+      'https://www.newzhongyuan.com/data/2017/g/www.gdxzytc.kenfor.com//db_file/201907/13/1563012549154999.jpg'
+    ], 
+    iconData:[{icon: '/images/commend.png', text: '强烈推荐'},
+              { icon: '/images/favourite.png', text: '我的收藏'},
+              { icon: '/images/money.png', text: '价格估算'},
+              { icon: '/images/history.png', text: '价格走势'}],
+    viewData: [{ picPath1: "http://www.monalisa.com.cn/UploadFile/products/e72ca080-918d-430c-baf3-83cf8021da44.jpg", text1: "新黑白根", picPath2: "http://www.monalisa.com.cn/UploadFile/products/419297cc-4c39-4d5c-a1d2-9497135e7c64.jpg", text2: "大漠流金"},
+      { picPath1: "http://www.monalisa.com.cn/UploadFile/products/c869276f-e9e1-456c-a792-28e87ca4bf45.jpg", text1: "顶级鱼肚白", picPath2: "http://www.monalisa.com.cn/UploadFile/products/5d8740e5-184f-49bd-bd7a-66ca17c9818f.jpg", text2: "新钻石流星" },
+      { picPath1: "http://www.monalisa.com.cn/UploadFile/products/d1c077fa-aadc-4e90-8038-b1d5252cdcad.jpg", text1: "新梦幻宝蓝", picPath2: "http://www.monalisa.com.cn/UploadFile/products/d1eeb6cd-d29e-4c05-9def-5cb8541b94c6.jpg", text2: "碧海云天" },
+      { picPath1: "http://www.monalisa.com.cn/UploadFile/products/65c7e70d-39f4-41b1-ab20-3306af2d72b8.jpg", text1: "江山如画", picPath2: "http://www.monalisa.com.cn/UploadFile/products/a3d84cd3-f140-4833-baea-5584373f5220.jpg", text2: "星尘灰" },
+      { picPath1: "", text1: "", picPath2: "", text2: "" },
+      { picPath1: "", text1: "", picPath2: "", text2: "" },
+      { picPath1: "", text1: "", picPath2: "", text2: "" },
+      { picPath1: "", text1: "", picPath2: "", text2: "" },
+      { picPath1: "", text1: "", picPath2: "", text2: "" },
+      { picPath1: "", text1: "", picPath2: "", text2: "" }
+    ]
   },
 
   /**
